4b8f0bd39d9ee29988a6714d83531f21a04b6699
Vimalate <1902644369@qq.com>
2022-05-06 21:39:30 +0800

    初始化vuex

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <h1>首页</h1>
+    <p>{{ store.state.count }}</p>
+    <p>{{ $store.state.count }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
+// import { useStore } from 'vuex'
+import { useStore } from '../../store/index'
+
+const store = useStore()
+console.log(store.state.count)
 
 </script>
 
